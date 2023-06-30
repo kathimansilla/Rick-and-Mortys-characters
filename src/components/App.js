459eae346429be4e3d1d619ac18300942933cc88
kathimansilla <mansillakatherina@gmail.com>
2023-06-30 19:47:05 +0200

    merge dev

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,35 @@
 import '../styles/App.scss';
-import {Route, Routes} from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import callToApi from '../services/api';
 
 function App() {
+// variables de estado
+const [characterList, setCharacterList] = useState([]);
+
+// funciones
+useEffect(() => {
+  callToApi()
+  .then((cleanData) => {
+    setCharacterList(cleanData);
+  });
+}, [])
+
+const renderCharacterList = (objList) => {
+  objList.map((eachElement) => {
+    return (
+    <li>
+      <img src={eachElement.image} alt={eachElement.name} />
+      <h3>{eachElement.name}</h3>
+      <p>{eachElement.species}</p>
+    </li>)
+  });
+};
   return (
     <div>
-      <h1>Hola mundo!</h1>
+      <h1>Hola</h1>
+      <ul>
+        {renderCharacterList(characterList)}
+      </ul>
     </div>
   );
 }
---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+const callToApi = () => {
+  return fetch('https://rickandmortyapi.com/api/character')
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data.results);
+      const cleanData = data.results.map((eachCharacter) => {
+        return {
+          name: eachCharacter.name,
+          image: eachCharacter.image,
+          species: eachCharacter.species,
+          id: eachCharacter.id,
+        };
+      });
+      return cleanData;
+    });
+};
+
+export default callToApi;
